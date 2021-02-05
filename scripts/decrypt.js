28daa598006f4a03561a2c3f757a5136cccc296b
Andrew Isherwood <andrew.isherwood@ptfs-europe.com>
2021-02-05 11:49:50 +0000

    Modify decrypt tool to prompt for key
    We don't want to be passing the key on the command line, this commit
    modifies the tool to prompt for it

--- scripts/decrypt.js
+++ scripts/decrypt.js
@@ -1,10 +1,19 @@
-const { decrypt }  = require('../helpers/encryption');
+const readline = require('readline');
+const { decrypt } = require('../helpers/encryption');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-(async () => {
+(() => {
 
-    const decrypted = await decrypt(process.argv[2]);
-    console.log(decrypted);
+    rl.question('Enter encryption key: ', async (key) => {
+        process.env.KEY = key;
+        const decrypted = await decrypt(process.argv[2]);
+        console.log(decrypted);
+        rl.close();
+    });
 
 })();
 
